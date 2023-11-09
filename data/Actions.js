847aaed558dedd34ea18cbc1fb6074d6cbc28c44
Mark Newman <mwnewman@umich.edu>
2023-11-08 22:07:15 -0500

    finding chats

--- data/Actions.js
+++ data/Actions.js
@@ -48,8 +48,45 @@ const addUser = (user) => {
   }
 }
 
-const addOrFindChat = (user1, user2) => {
-  
+const addOrSelectChat = (user1id, user2id) => {
+ 
+  return async (dispatch) => {
+    
+    const chatQuery = query(collection(db, 'chats'),
+      where('participants', 'array-contains', user1id),
+    );
+    const results = await getDocs(chatQuery);
+    /*
+      Ideally we would do this:
+      const chatQuery = query(
+        collection(db, 'chats'),
+        where('participants', 'array-contains', user1id),
+        where('participants', 'array-contains', user2id)
+      );
+      but Firestore doesn't allow more than one 'array-contains'
+      where clauses in a single query. So instead we do the 
+      second 'array-contains' clause "manually"
+    );
+    */
+    chatSnap = results.docs?.find(
+        elem=>elem.data().participants.includes(user2id));
+    let theChat;
+
+    if (!chatSnap) { //; we didn't find a match, create a new one
+      theChat = {
+        participants: [user1id, user2id],
+      }
+      const chatRef = await addDoc(collection(db, 'chats'), theChat);
+      theChat.id = chatRef.id
+      console.log('created a new chat:', theChat);
+    } else { // we did find a match, so let's use it.
+      theChat = {
+        ...chatSnap.data(),
+        id: chatSnap.id
+      }
+      console.log('found a matching chat:', theChat);
+    }
+  }
 }
 
-export { addUser, subscribeToUserUpdates }
+export { addUser, subscribeToUserUpdates, addOrSelectChat }
